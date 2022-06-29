139db3156496285307cf08b9ca4cf79674d3baf9
omniahalharthi <94142998+omniahalharthi@users.noreply.github.com>
2022-06-29 11:44:20 +0300

    Update conflict.php
    change on line 5

--- conflict.php
+++ conflict.php
@@ -2,6 +2,21 @@
 echo 'push test';
 echo 'change 1';
 echo 'change 2';
-echo 'change 3';
+$favcolor = "red";
+
+switch ($favcolor) {
+  case "red":
+    echo "Your favorite color is red!";
+    break;
+  case "blue":
+    echo "Your favorite color is blue!";
+    break;
+  case "green":
+    echo "Your favorite color is green!";
+    break;
+  default:
+    echo "Your favorite color is neither red, blue, nor green!";
+}
 echo 'created with git bash';
+
 ?>
