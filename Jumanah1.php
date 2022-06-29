b1f07f78193e336e8c6f39d49391148712e449b2
Afnan Amer <AfnanAmirAltalhi@gmail.com>
2022-06-29 13:46:10 +0300

    DELETE unneeded files #39

--- Jumanah1.php
+++
@@ -1,4 +0,0 @@
-<?php
-echo "Hello Friends";
-echo "Final test";
-?>
--- tasneem_test_push.php
+++
@@ -1,7 +0,0 @@
-<?php
-echo 'push test';
-echo 'change 1';
-echo 'change 2';
-echo 'change 3';
-echo 'created with git bash';
-?>
