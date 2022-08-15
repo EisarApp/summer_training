ac9b121f5c8d5522c52c1449ee8e76c2b148931f
Wojood <wojood.qmatrafi@gmail.com>
2022-08-15 22:24:35 +0300

    plan
    FIX

--- Eisar_training/resources/views/plan.blade.php
+++ Eisar_training/resources/views/plan.blade.php
@@ -76,7 +76,7 @@
                                 <td class="px-5 py-4">
                                     <div class="flex items-center space-x-4 text-sm">
                                         <button
-                                            class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-500 focus:outline-none focus:shadow-outline-gray hover:bg-indigo-600"
+                                            class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-500 focus:outline-none focus:shadow-outline-gray"
                                             aria-label="Edit">
                                             <svg class="w-5 h-5" aria-hidden="true" fill="currentColor"
                                                 viewBox="0 0 20 20">
@@ -86,7 +86,7 @@
                                             </svg>
                                         </button>
                                         <button
-                                            class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-500 focus:outline-none focus:shadow-outline-gray hover:bg-red-500"
+                                            class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-500 focus:outline-none focus:shadow-outline-gray"
                                             aria-label="Delete">
                                             {{-- <svg class="w-5 h-5" aria-hidden="true" fill="currentColor"
                                                 viewBox="0 0 20 20">
