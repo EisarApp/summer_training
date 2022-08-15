3c096be1b1d46d880d3b810ccb28883215b1c245
Wojood <wojood.qmatrafi@gmail.com>
2022-08-15 21:26:19 +0300

    Plan page don
    done for plan

--- Eisar_training/resources/views/plan.blade.php
+++ Eisar_training/resources/views/plan.blade.php
@@ -9,9 +9,9 @@
     <div class="container mx-auto px-4 sm:px-8">
         <div class="py-8">
             <div class="my-2 flex sm:flex-row flex-col">
-                {{-- search input --}}
+                {{-- filtter input  if exite--}}
                 <div class="block relative">
-                   
+
                 </div>
 
             </div>
@@ -37,7 +37,7 @@
                                     نوع التدريب
                                 </th>
                                 <th class="px-5 py-4">
-                                    f
+
                                 </th>
 
                             </tr>
@@ -72,8 +72,30 @@
                                     </p>
                                 </td>
 
-                                <td class="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                                    {{-- icons here --}}
+                            
+                                <td class="px-5 py-4">
+                                    <div class="flex items-center space-x-4 text-sm">
+                                        <button
+                                            class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-500 focus:outline-none focus:shadow-outline-gray hover:bg-indigo-600"
+                                            aria-label="Edit">
+                                            <svg class="w-5 h-5" aria-hidden="true" fill="currentColor"
+                                                viewBox="0 0 20 20">
+                                                <path
+                                                    d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z">
+                                                </path>
+                                            </svg>
+                                        </button>
+                                        <button
+                                            class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-500 focus:outline-none focus:shadow-outline-gray hover:bg-red-500"
+                                            aria-label="Delete">
+                                            <svg class="w-5 h-5" aria-hidden="true" fill="currentColor"
+                                                viewBox="0 0 20 20">
+                                                <path fill-rule="evenodd"
+                                                    d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                                                    clip-rule="evenodd"></path>
+                                            </svg>
+                                        </button>
+                                    </div>
                                 </td>
                             </tr>
 
@@ -82,8 +104,10 @@
 
                 </div>
             </div>
-            <div class="">
-            <button type="button" class="mb-2 w-64 px-6 py-2.5 bg-indigo-900 text-white font-medium text-xs leading-normal uppercase rounded shadow-md hover:bg-indigo-800 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">إضافة خطة</button>
+            <div>
+                <button type="button"
+                    class="mb-2 w-64 px-6 py-2.5 bg-indigo-900 text-white font-medium text-xs leading-normal uppercase rounded shadow-md hover:bg-indigo-800 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">إضافة
+                    خطة</button>
             </div>
         </div>
     </div>
