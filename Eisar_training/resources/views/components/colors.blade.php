a48692ea2c9cdf0a23c9dfbc86ee54a789171916
Tasneem Ali <tasneemalialzahrani@outlook.com>
2022-08-16 11:46:00 +0300

    Created a component for colors

---
+++ Eisar_training/resources/views/components/colors.blade.php
@@ -0,0 +1,15 @@
+<script src="https://cdn.tailwindcss.com"></script>
+<script>
+    tailwind.config = {
+        theme: {
+            extend: {
+                colors: {
+                    orange: '#FF6663',
+                    orangelight: '#FF918E',
+                    blue: '#374782',
+                    bluedark: '#061D70'
+                }
+            }
+        }
+    }
+</script>
