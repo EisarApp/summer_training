c4ee3ad450dab8dc432a8bf84a73cb338f998a60
Wojood <wojood.qmatrafi@gmail.com>
2022-08-16 12:11:51 +0300

    added new color in my branch

---
+++ Eisar_training/resources/views/colors.blade.php
@@ -0,0 +1,17 @@
+<script src="https://cdn.tailwindcss.com"></script>
+<script>
+    tailwind.config = {
+        theme: {
+            extend: {
+                colors: {
+                    orange: '#FF6663',
+                    orangelight: '#FF918E',
+                    blue: '#374782',
+                    bluedark: '#061D70'.
+                    eisar: '#FD4F42'
+
+                }
+            }
+        }
+    }
+</script>
