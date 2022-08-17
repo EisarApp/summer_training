926edec9a6713c8eee6c5ad405ea6da1069bf3b1
Wojood <wojood.qmatrafi@gmail.com>
2022-08-17 21:58:36 +0300

    added all the tables

--- Eisar_training/resources/views/employeesAcces.blade.php
+++ Eisar_training/resources/views/employeesAcces.blade.php
@@ -1,7 +1,7 @@
 <html dir="rtl">
 
 <head>
-   <x-head/>
+    <x-head />
 </head>
 
 
@@ -11,10 +11,8 @@
             <div class="my-2 flex sm:flex-row flex-col float-left">
                 {{-- filtter input if exite--}}
                 <div class="block">
-                    <button type="button"
-                        class="inline-block float--left mr-2 px-4 py-2.5 bg-orange-500 text-white font-medium text-xs leading-tight rounded shadow-md hover:bg-orange-800 hover:shadow-lg focus:bg-orange-800 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-orange-800 active:shadow-lg transition duration-150 ease-in-out">إضافة
-                        موظف جديد
-                    </button>
+                    <x-table-button>إضافة موظف جديد</x-table-button>
+
                 </div>
 
 
@@ -23,19 +21,15 @@
             <div class="my-2 flex sm:flex-row flex-col">
                 {{-- search input --}}
                 <div class="block relative">
-                    <span class="h-full absolute inset-y-0 left-0 flex items-center pl-2">
-                        <svg viewBox="0 0 24 24" class="h-4 w-4 fill-current text-gray-500">
-                            <path
-                                d="M10 4a6 6 0 100 12 6 6 0 000-12zm-8 6a8 8 0 1114.32 4.906l5.387 5.387a1 1 0 01-1.414 1.414l-5.387-5.387A8 8 0 012 10z">
-                            </path>
-                        </svg>
-                    </span>
-                    <input placeholder="Search"
-                        class="appearance-none rounded-r rounded-l sm:rounded-l-none border border-gray-400 border-b block pl-8 pr-6 py-2 w-full bg-white text-sm placeholder-gray-400 text-gray-700 focus:bg-white focus:placeholder-gray-600 focus:text-gray-700 focus:outline-none" />
+                    <x-search-input>ابحث باسم الموظف</x-search-input>
                 </div>
 
             </div>
-            <x-table :headers="['اسم الخطة','القسم']">
+            {{-- temporary --}}
+            @php
+            $Users=array('pen'=>'33','jhon'=>'55');
+            @endphp
+            <x-table :headers="['الرقم','اسم الموظف','الصلاحيات','المسمى الوظيفي','القسم','الخطة']">
                 @foreach($Users as $user=> $data)
                 <tr>
 
@@ -45,6 +39,19 @@
                     </x-table-data>
                     <x-table-data>
                         {{$data;}}
+                    </x-table-data>
+
+                    <x-table-data>
+                        {{-- the last column --}}
+                        <div class="my-2 flex sm:flex-row flex-col">
+
+                            <div class="inline-flex relative">
+                                <x-table-button>تعديل الصلاحية</x-table-button>
+                                <x-table-button> عرض</x-table-button>
+                                <x-table-button> تغير كلمة المرور</x-table-button>
+                            </div>
+
+                        </div>
                     </x-table-data>
                 </tr>
 
--- Eisar_training/resources/views/students.blade.php
+++ Eisar_training/resources/views/students.blade.php
@@ -1,7 +1,7 @@
 <html dir="rtl">
 
 <head>
- <x-head/>
+    <x-head />
 </head>
 {{-- <style>
     .dropdown:hover>.dropdown-content {
@@ -17,21 +17,19 @@
             <div class="my-2 flex sm:flex-row flex-col float-left">
                 {{-- filtter input if exite--}}
                 <div class="block relative">
-                    <button type="button"
-                        class="inline-block mr-2 float-left px-4 py-2.5 bg-orange-500 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-orange-800 hover:shadow-lg focus:bg-orange-800 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-orange-800 active:shadow-lg transition duration-150 ease-in-out">إضافة
-                        طالب جديد
-                    </button>
-                    <button type="button"
-                        class="inline-block mr-2 float-left px-4 py-2.5 bg-orange-500 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-orange-800 hover:shadow-lg focus:bg-orange-800 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-orange-800 active:shadow-lg transition duration-150 ease-in-out">إستراد
-                        طلاب
-                    </button>
+                    <x-table-button> إضافة طالب جديد </x-table-button>
+                    <x-table-button> إستراد طلاب </x-table-button>
+
                 </div>
 
             </div>
-
             <div class="my-2 flex sm:flex-row flex-col">
-                {{-- search input --}}
+                {{-- dropdown input --}}
+                <div class="block relative">
+                    <x-search-input>ابحث باسم الطالب</x-search-input>
 
+                </div>
+                {{-- change the dropdow --}}
                 <div class="block relative mx-2 " x-data="{open:false}">
                     <button
                         class="appearance-none rounded border border-gray-400 border-b block pl-8 pr-6 py-2 w-full bg-white text-sm placeholder-gray-400 text-gray-700 focus:bg-white focus:placeholder-gray-600 focus:text-gray-700 focus:outline-none"
@@ -78,7 +76,11 @@
                 </div>
 
             </div>
-            <x-table :headers="['اسم الخطة','القسم']">
+            {{-- temporary --}}
+            @php
+            $Users=array('pen'=>'33','jhon'=>'55');
+            @endphp
+            <x-table :headers="['اسم الطالب','الرقم الجامعي','اسم الجهة','اسم مسؤول الجهة','الشعبة']">
                 @foreach($Users as $user=> $data)
                 <tr>
 
