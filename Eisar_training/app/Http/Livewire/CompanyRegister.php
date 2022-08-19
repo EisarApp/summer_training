7378231c92ebb1896a9a1dbe029f26903bbc99fd
BshayerAbdullah <94211947+BshayerAbdullah@users.noreply.github.com>
2022-08-20 01:58:38 +0300

    Some Edit

--- Eisar_training/app/Http/Livewire/CompanyRegister.php
+++ Eisar_training/app/Http/Livewire/CompanyRegister.php
@@ -8,6 +8,7 @@ use App\Models\City;
 use App\Models\Region;
 use App\Models\University;
 use App\Models\Department;
+use App\Models\UserCompany;
 
 class CompanyRegister extends Component
 {
@@ -24,51 +25,54 @@ class CompanyRegister extends Component
     public $confirmPassword;
     public $gender;
     public $mobile;
-
+    public $logo;
+    public $header;
+    public $description;
+    public $domain;
+    public $facility_size;
 
     //drop-down properties region-city
     public $region;
     public $cities = [];
     public $city;
 
-    //drop-down properties uni-dep
-    public $university;
-    public $departments = [];
-    public $department;
-
-
-
-
     //Model Binding
     public $trainerType;
 
-    // private $validationRules = [
-    //     1 => [
-    //         'firstName' => ['required', 'min:3'],
-    //         'lastName' => ['required', 'min:3'],
-    //         'email' => ['required', 'email', 'unique:users,email'],
-    //     ],
-    //     2 => [
-    //         'password' => ['required', 'string', 'min:8'],
-    //         'confirmPassword' => ['required', 'string', 'same:password', 'min:8'],
-    //     ],
-    // ];
-
+    public function submit()
+    {
+        $user_uni=User::create([
+            'name' => "{$this->firstName} {$this->lastName}",
+            'type_id' => $this->userType,
+            'email' => $this->email,
+            'password' => bcrypt($this->password),
+            'gender' => $this->gender,
+            'mobile' => $this->mobile,
+            'is_active'=>1,
+            'university_id' => $this->universityId,
+            'department_id' => $this->departmentId,
+        ]);
+        UserCompany::create([
+            'user_id' =>$user_uni->id,
+            'logo' => $this->logo,
+            'header' => $this->header,
+            'description' => $this->description,
+            'domain' => $this->domain,
+            'city_id' => $this->city,
+            'region_id' => $this->region,
+            'facility_size' => $this->facility_size,
+        ]);
+    }
 
     public function render()
     {
         if (!empty($this->region)) {
             $this->cities = City::where('region_id', $this->region)->get();
         }
-        if (!empty($this->university)) {
-            $this->departments = Department::where('university_id', $this->university)->get();
-        } 
-        
+    
         $regions = Region::orderBy('name')->get();
-        $universities = University::orderBy('name')->get();
         return view('livewire.company-register')->with([
             'regions' => $regions,
-            'universities'=>$universities,
         ]);
     }
 }
--- Eisar_training/resources/views/livewire/trainee-register.blade.php
+++ Eisar_training/resources/views/livewire/trainee-register.blade.php
@@ -1,6 +1,6 @@
 <div>
     dd({{$userType}});
-    
+
     <form wire:submit.prevent="submit" novalidate="" action=""
         class=" w-auto lg:w-[50rem] container flex-col mx-auto space-y-12 ng-untouched ng-pristine ng-valid">
         <fieldset class="grid grid-cols-4 gap-6 p-6 rounded-md shadow-sm bg-gray-100">
@@ -15,12 +15,80 @@
                 <x-form.input label="الجوال" name="mobile" />
                 <x-form.input label="كلمة المرور" name="password" type="password" />
                 <x-form.input label="تأكيد كلمة المرور" name="confirmPassword" type="password" />
+                {{--
+                <x-form.field /> --}}
+
+                <!--  dropdown region and city -->
+                <x-form.form-dropdown label="المنطقة" name="region">
+                    <option selected disabled>اختر المنطقة</option>
+                    @foreach($regions as $region)
+                    <option value={{ $region ->id }}>{{ $region ->name }}</option>
+                    @endforeach
+                </x-form.form-dropdown>
+
+                @if(count($cities) > 0)
+                <x-form.form-dropdown label="المدينة" name="city">
+                    <option selected disabled>اختر المدينة</option>
+                    @foreach($cities as $city)
+                    <option value={{ $city ->id }}>{{ $city ->name }}</option>
+                    @endforeach
+                </x-form.form-dropdown>
+                @else
+                <x-form.field />
+                @endif
+
+                <!--  dropdown university and department -->
+                <x-form.form-dropdown label="الجامعة" name="university">
+                    <option selected disabled>اختر الجامعة</option>
+                    @foreach($universities as $university)
+                    <option value={{ $university ->id }}>{{ $university ->name }}</option>
+                    @endforeach
+                </x-form.form-dropdown>
+
+                @if(count($departments) > 0)
+                <x-form.form-dropdown label="الكلية" name="department">
+                    <option selected disabled>اختر الكلية</option>
+                    @foreach($departments as $department)
+                    <option value={{ $department ->id }}>{{ $department ->name }}</option>
+                    @endforeach
+                </x-form.form-dropdown>
+                @else
+                <x-form.field />
+                @endif
+
+                <!--  dropdown gender -->
                 <x-form.form-dropdown label="الجنس" name="gender">
                     <option selected disabled>اختر الجنس</option>
                     <option value="f">أنثى</option>
                     <option value="m">ذكر</option>
                 </x-form.form-dropdown>
+
+                <x-form.input label="التخصص" name="major" />
+                <x-form.input label="سنة التخرج" name="graduation_year" />
                 <x-form.field />
+
+                <x-form.form-dropdown label="نوع المعدل" name="gpa_type">
+                    <option selected disabled>اختر نوع المعدل</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                </x-form.form-dropdown>
+                <x-form.input label="المعدل" name="gpa" />
+
+                <x-form.field>
+                    <x-form.label label="السجل الأكاديمي" name="academic_transaction" />
+                    <div class="flex">
+                        <input type="file" name="academic_transaction" id="files"
+                            class="px-8 py-12 border-2 border-dashed rounded-md dark:border-gray-700 dark:text-gray-400 dark:bg-gray-800">
+                    </div>
+                </x-form.field>
+
+                <x-form.field>
+                    <x-form.label label="السيرة الذاتية" name="cv" />
+                    <div class="flex">
+                        <input type="file" name="cv" id="files"
+                            class="px-8 py-12 border-2 border-dashed rounded-md dark:border-gray-700 dark:text-gray-400 dark:bg-gray-800">
+                    </div>
+                </x-form.field>
 
                 @if(! $traineeType)
                 <div class="flex py-3 ">
@@ -31,55 +99,9 @@
 
                 @if($traineeType == "طالب")
 
-                <!--  dropdown region and city -->
-                <x-form.form-dropdown label="المنطقة" name="region">
-                    <option selected disabled>اختر المنطقة</option>
-                    @foreach($regions as $region)
-                    <option value={{ $region ->id }}>{{ $region ->name }}</option>
-                    @endforeach
-                </x-form.form-dropdown>
-
-                @if(count($cities) > 0)
-                <x-form.form-dropdown label="المدينة" name="city">
-                    <option selected disabled>اختر المدينة</option>
-                    @foreach($cities as $city)
-                    <option value={{ $city ->id }}>{{ $city ->name }}</option>
-                    @endforeach
-                </x-form.form-dropdown>
-                @else
-                <x-form.field />
-                @endif
-
-                <!--  dropdown university and department -->
-                <x-form.form-dropdown label="الجامعة" name="university">
-                    <option selected disabled>اختر الجامعة</option>
-                    @foreach($universities as $university)
-                    <option value={{ $university ->id }}>{{ $university ->name }}</option>
-                    @endforeach
-                </x-form.form-dropdown>
-
-                @if(count($departments) > 0)
-                <x-form.form-dropdown label="الكلية" name="department">
-                    <option selected disabled>اختر الكلية</option>
-                    @foreach($departments as $department)
-                    <option value={{ $department ->id }}>{{ $department ->name }}</option>
-                    @endforeach
-                </x-form.form-dropdown>
-                @else
-                <x-form.field />
-                @endif
-
-
-                <x-form.input label="التخصص" name="major" />
                 <x-form.input label="الرقم الجامعي" name="studentNumber" />
-                <x-form.input label="سنة التخرج" name="graduation_year" />
-                <x-form.field />
-                <x-form.form-dropdown label="نوع المعدل" name="gpa_type">
-                    <option selected disabled>اختر نوع المعدل</option>
-                    <option value="4">4</option>
-                    <option value="5">5</option>
-                </x-form.form-dropdown>
-                <x-form.input label="المعدل" name="gpa" />
+                {{--
+                <x-form.field /> --}}
                 <x-form.input label="ساعات التدريب" name="trainingHours" />
                 <x-form.form-dropdown label="فترة التدريب" name="training_date">
                     <option selected disabled>اختر فترة التدريب</option>
@@ -87,23 +109,6 @@
                     <option value="secondsemester">Secondsemester</option>
                     <option value="thirdsemester">Thirdsemester</option>
                 </x-form.form-dropdown>
-
-                <x-form.field>
-                    <x-form.label label="السجل الأكاديمي" name="academic_transaction" />
-                    <div class="flex">
-                        <input type="file" name="academic_transaction" id="files"
-                            class="px-8 py-12 border-2 border-dashed rounded-md dark:border-gray-700 dark:text-gray-400 dark:bg-gray-800">
-                    </div>
-                </x-form.field>
-
-                <x-form.field>
-                    <x-form.label label="السيرة الذاتية" name="cv" />
-                    <div class="flex">
-                        <input type="file" name="cv" id="files"
-                            class="px-8 py-12 border-2 border-dashed rounded-md dark:border-gray-700 dark:text-gray-400 dark:bg-gray-800">
-                    </div>
-                </x-form.field>
-                <x-form.field />
 
                 <x-form.field>
                     <div class=" flex items-center justify-between px-4 py-10 text-right sm:px-6">
@@ -113,66 +118,6 @@
 
                 @elseif($traineeType == "خريج")
 
-                <!--  dropdown region and city -->
-                <x-form.form-dropdown label="المنطقة" name="region">
-                    <option selected disabled>اختر المنطقة</option>
-                    @foreach($regions as $region)
-                    <option value={{ $region ->id }}>{{ $region ->name }}</option>
-                    @endforeach
-                </x-form.form-dropdown>
-
-                @if(count($cities) > 0)
-                <x-form.form-dropdown label="المدينة" name="city">
-                    <option selected disabled>اختر المدينة</option>
-                    @foreach($cities as $city)
-                    <option value={{ $city ->id }}>{{ $city ->name }}</option>
-                    @endforeach
-                </x-form.form-dropdown>
-                @else
-                <x-form.field />
-                @endif
-
-                <!--  dropdown university and department -->
-                <x-form.form-dropdown label="الجامعة" name="university">
-                    <option selected disabled>اختر الجامعة</option>
-                    @foreach($universities as $university)
-                    <option value={{ $university ->id }}>{{ $university ->name }}</option>
-                    @endforeach
-                </x-form.form-dropdown>
-
-                @if(count($departments) > 0)
-                <x-form.form-dropdown label="الكلية" name="department">
-                    <option selected disabled>اختر الكلية</option>
-                    @foreach($departments as $department)
-                    <option value={{ $department ->id }}>{{ $department ->name }}</option>
-                    @endforeach
-                </x-form.form-dropdown>
-                @else
-                <x-form.field />
-                @endif
-
-
-                <x-form.input label="التخصص" name="major" />
-                <x-form.field />
-                <x-form.form-dropdown label="نوع المعدل" name="gpa_type">
-                    <option selected disabled>اختر نوع المعدل</option>
-                    <option value="4">4</option>
-                    <option value="5">5</option>
-                </x-form.form-dropdown>
-                <x-form.input label="المعدل" name="gpa" />
-                <x-form.input label="ساعات التدريب" name="trainingHours" />
-                <x-form.form-dropdown label="فترة التدريب" name="training_date">
-                    <option selected disabled>اختر فترة التدريب</option>
-                    <option value="firstsemester">Firstsemester</option>
-                    <option value="secondsemester">Secondsemester</option>
-                    <option value="thirdsemester">Thirdsemester</option>
-                </x-form.form-dropdown>
-                <x-form.form-dropdown label="الدرجة العلمية" name="academic_degree">
-                    <option selected disabled>الدرجة العلمية</option>
-                    <option value="bachelor">بكالوريوس</option>
-                    <option value="diploma">دبلوم</option>
-                </x-form.form-dropdown>
-
                 <x-form.field>
                     <x-form.label label="وثيقة التخرج" name="graduation_certificate" />
                     <div class="flex">
@@ -180,23 +125,6 @@
                             class="px-8 py-12 border-2 border-dashed rounded-md dark:border-gray-700 dark:text-gray-400 dark:bg-gray-800">
                     </div>
                 </x-form.field>
-
-                <x-form.field>
-                    <x-form.label label="السجل الأكاديمي" name="academic_transaction" />
-                    <div class="flex">
-                        <input type="file" name="academic_transaction" id="files"
-                            class="px-8 py-12 border-2 border-dashed rounded-md dark:border-gray-700 dark:text-gray-400 dark:bg-gray-800">
-                    </div>
-                </x-form.field>
-
-                <x-form.field>
-                    <x-form.label label="السيرة الذاتية" name="cv" />
-                    <div class="flex">
-                        <input type="file" name="cv" id="files"
-                            class="px-8 py-12 border-2 border-dashed rounded-md dark:border-gray-700 dark:text-gray-400 dark:bg-gray-800">
-                    </div>
-                </x-form.field>
-                <x-form.field />
 
                 <x-form.field>
                     <div class=" flex items-center justify-between px-4 py-10 text-right sm:px-6">
