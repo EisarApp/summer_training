6f323f1da707c6c6ffa0606cda080898e0b21e17
JumanahMt <94211785+JumanahMutahhar@users.noreply.github.com>
2022-08-23 05:12:27 -0700

    university dashboard

---
+++ Eisar_training/resources/views/components/university/university-dashboard.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="ar" dir="rtl">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>university</title>
+    <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script>
+    <script src="https://code.iconify.design/iconify-icon/1.0.0-beta.3/iconify-icon.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
+    <script src="//unpkg.com/alpinejs" defer></script>
+    <x-colors />
+</head>
+
+<body>
+    <!-- if the user is auth -->
+    <div class="grid grid-cols-5">
+        <x-university.university-side-menu />
+        <div class="col-span-4 pr-8">
+            <x-university.university-header />
+           
+            <div class="grid grid-cols-2 mt-10">
+                <div>
+                    <h1>المهام</h1>
+                    <x-student.student-tasks-card color="border-orange-500" />
+                    <x-student.student-tasks-card color="border-orange-500" />
+                    <x-student.student-tasks-card color="border-orange-500" />
+                </div>
+                <div>
+                    <h1>الاحصائيات الاسبوعية</h1>
+                    <x-chart />
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
---
+++ Eisar_training/resources/views/components/university/university-header.blade.php
@@ -0,0 +1,25 @@
+
+
+<nav {{ $attributes(['class'=> 'py-4 ']) }}>
+    <div x-data="{ show: false }" class="max-w-7xl mx-auto">
+        <div class=" flex items-center justify-between h-16">
+            {{-- TODO: use alpine --}}
+            {{-- main menu --}}
+            <div class=" flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+                <div class="sm:block sm:ml-6">
+                    <div class="">
+                        
+                        <!-- قائمة الفرص  -->
+                        
+                            <h1 class="text-gray-800 "> مرحبا بك سلطان   </h1>
+                            
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+
+
+</nav>
